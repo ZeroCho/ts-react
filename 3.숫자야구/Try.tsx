09c4d225eeaf5f1f91ea035db26212911b2721dc
ZeroCho <zerohch0@gmail.com>
2020-11-06 18:09:51 +0900

    Fix: wrap try component with memo

--- 3.숫자야구/Try.tsx
+++ 3.숫자야구/Try.tsx
@@ -1,13 +1,14 @@
+import { memo } from 'react';
 import * as React from 'react';
 import { TryProps } from './types';
 
-const Try: React.FunctionComponent<TryProps> = ({ tryInfo }) => {
+const Try: React.FunctionComponent<TryProps> = memo(({ tryInfo }) => {
   return (
     <li>
       <div>{tryInfo.try}</div>
       <div>{tryInfo.result}</div>
     </li>
   );
-};
+});
 
 export default Try;
